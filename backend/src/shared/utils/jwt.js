321e131547839049c8ba87ae7c67a673a6699010
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-02-11 17:54:41 +0530

    push the code

--- backend/src/shared/utils/jwt.js
+++ backend/src/shared/utils/jwt.js
@@ -4,13 +4,13 @@ exports.generateTokens = (userId) => {
   const accessToken = jwt.sign(
     { id: userId },
     process.env.JWT_ACCESS_SECRET,
-    { expiresIn: '15m' } // 15 minutes validity
+    { expiresIn: '15m' } //15 minutes validity
   );
 
   const refreshToken = jwt.sign(
     { id: userId },
     process.env.JWT_REFRESH_SECRET,
-    { expiresIn: '7d' } // 7 days validity
+    { expiresIn: '7d' } //7 days validity
   );
 
   return { accessToken, refreshToken };
