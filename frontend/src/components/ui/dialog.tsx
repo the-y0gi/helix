c93c0f646a413db75e9149e165ccda84eb02d61a
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2026-04-25 13:48:24 +0530

    littlebitchanges-new-features

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -85,7 +85,7 @@ function DialogContent({
           }
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-10"
+            className="ring-offset-background focus:ring-ring absolute top-4 right-4 z-[1010] rounded-full p-1 opacity-70 transition-all hover:opacity-100 hover:bg-zinc-100/50 dark:hover:bg-zinc-800/50 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none drop-shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-8 text-zinc-800 dark:text-zinc-200"
           >
             <XIcon />
             <span className="sr-only">Close</span>
